565ba0c035460086d9f8ceec015f22347633412c
Nikolai Guyot <nikolai.guyot@code.berlin>
2022-01-27 19:33:46 +0100

    Install dependencies based on Ubuntu version

--- install-swift.sh
+++ install-swift.sh
@@ -14,9 +14,21 @@ TOOLCHAIN_TAR_URL="$TOOLCHAIN_BASE_URL/$TOOLCHAIN_TAR"
 TOOLCHAIN_SIG_URL="$TOOLCHAIN_BASE_URL/$TOOLCHAIN_SIG"
 
 echo "Installing system dependencies 📦"
-sudo apt-get install \
-binutils git gnupg2 libc6-dev libcurl4 libedit2 libgcc-9-dev libpython2.7 libsqlite3-0 libstdc++-9-dev libxml2 \
-libz3-dev pkg-config tzdata uuid-dev zlib1g-dev
+if [ $UBUNTU_VERSION == "18.04" ]; then
+    echo "choosing list for 18.04"
+    sudo apt-get install \
+    binutils git libc6-dev libcurl4 libedit2 libgcc-5-dev libpython2.7 libsqlite3-0 libstdc++-5-dev libxml2 \
+    pkg-config tzdata zlib1g-dev
+elif [ $UBUNTU_VERSION == "20.04" ]; then
+    echo "choosing list for 20.04"
+    sudo apt-get install \
+    binutils git gnupg2 libc6-dev libcurl4 libedit2 libgcc-9-dev libpython2.7 libsqlite3-0 libstdc++-9-dev libxml2 \
+    libz3-dev pkg-config tzdata uuid-dev zlib1g-dev
+else
+    echo "No Swift Toolchain available for Ubuntu version '$UBUNTU_VERSION'."
+    echo "Visit https://swift.org/download for more information."
+    exit 1;
+fi
 
 if [ -d "/usr/share/swift-toolchain" ]; then
     echo "Toolchain already exists, skipping download ✅"
